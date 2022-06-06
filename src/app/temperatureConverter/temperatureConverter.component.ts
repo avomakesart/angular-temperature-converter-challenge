841ea664523f081d00787a2e82920ce108131024
Elvis Enrique Montezuma Villegas <elvismontezuma@hotmail.com>
2022-06-06 14:46:29 -0500

    Challenge completed

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -10,7 +10,32 @@ export class TemperatureConverter implements OnInit {
 
   constructor() {}
 
+  celsius: number = null;
+  fahrenheit: number = null;
+
   ngOnInit() {}
 
+  conversion(from: string) {
+    if (from === 'c') {
+      if (this.celsius === null) {
+        this.fahrenheit = null;
+        return;
+      }
+      this.fahrenheit = (this.celsius * 9 / 5) + 32;
+      if (Math.floor(this.fahrenheit) === this.fahrenheit) {
+        this.fahrenheit = Number(this.fahrenheit.toFixed(1));
+      }
+      
+    } else {
+      if (this.fahrenheit === null) {
+        this.celsius = null;  
+        return;
+      }
+      this.celsius = (this.fahrenheit - 32) *  5 / 9;
+      if (Math.floor(this.celsius) !== this.celsius) {
+        this.celsius = Number(this.celsius.toFixed(1));
+      }      
+    }
+  }
 
 }
